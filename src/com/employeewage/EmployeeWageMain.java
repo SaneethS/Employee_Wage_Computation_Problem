4423e5efedb9819c40a4f61f555cb80e9aaaec9e
Saneeth S <saneeths@125AMBBLR-Chethan-RV.local>
2021-08-21 20:23:19 +0530

    Calculated daily wage of employee

--- src/com/employeewage/EmployeeWageMain.java
+++ src/com/employeewage/EmployeeWageMain.java
@@ -5,11 +5,15 @@ public class EmployeeWageMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage computation problem");
 		
+		//UC1
 		Employee emp = new Employee();
 		if(emp.isPresent())
 			System.out.println("Employee is present");
 		else
 			System.out.println("Employee is absent");
+		
+		//UC2
+		System.out.println("The Daily Wage of Employee : "+emp.daily_wage);
 		
 
 	}
