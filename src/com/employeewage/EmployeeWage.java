b4b13b9d7c9385067dafbcf0573b00fc315ef38c
Saneeth S <saneeths@125AMBBLR-Chethan-RV.local>
2021-08-21 19:31:05 +0530

    Welcome to Employee wage computation added

---
+++ src/com/employeewage/EmployeeWage.java
@@ -0,0 +1,10 @@
+package com.employeewage;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		System.out.println("Welocome to Employee Wage computation problem");
+
+	}
+
+}
