d9c718d039ec1cdc4ff89efc04cea7d39f8e4422
Saneeth S <saneeths@125AMBBLR-Chethan-RV.local>
2021-08-23 11:46:01 +0530

    Changes to the variable name in UC1

--- src/com/employeewage/Employee.java
+++ src/com/employeewage/Employee.java
@@ -3,8 +3,8 @@ package com.employeewage;
 public class Employee {
 
 	public boolean isPresent() {
-		double emp_attn=(int)Math.floor(Math.random()*10)%2;
-		if(emp_attn==1) 
+		double empAttn=(int)Math.floor(Math.random()*10)%2;
+		if(empAttn==1) 
 			return true;
 		else
 			return false;
